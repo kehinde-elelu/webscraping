9f264b7f852de95e4b9e198efbabce9f6a66210c
kehinde-elelu <abdulsalamelelu@gmail.com>
2024-08-04 09:59:21 -0500

    using driver

--- 1_bfsoup.py
+++ 1_bfsoup.py
@@ -1,4 +1,11 @@
-import undetected_chromedriver as uc
+'''
+Reference: 
+https://www.scrapingbee.com/blog/undetected-chromedriver-python-tutorial-avoiding-bot-detection/
+https://github.com/ultrafunkamsterdam/nodriver
+https://ultrafunkamsterdam.github.io/nodriver/nodriver/quickstart.html#usage-example
+'''
+
+
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -6,28 +13,38 @@ import time
 import random
 import csv
 import os
+import asyncio
+import nodriver as uc
 
-# Set up undetected_chromedriver with Selenium
-options = webdriver.ChromeOptions()
-options.add_argument("https:www.google.com")
-driver = uc.Chrome(options=options)
+base_path = "/Users/kehindeelelu/Documents/ETS/webscraping/data"
 
-job_listings = []
+stateNames = ['Arkansas']
+#   'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut',
+#   'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas',
+#   'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 
+#   'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 
+#   'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon',
+#   'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Utah', 
+#   'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+# ]
 
-def scrapping_website(url):
-    try:
-        driver.get(url)
-        
-        # Add a random delay to mimic human behavior
-        time.sleep(random.uniform(1, 3))
-        
-        # Ensure the page has fully loaded
-        time.sleep(5)  # Increase if the site loads slowly
-        
+for state in stateNames:
+    os.makedirs(f'{base_path}/html_files/{state}/', exist_ok=True)
+
+    base_url = f"https://www.careerbuilder.com/jobs?location={state}&page_number="
+    start_number = 0
+
+    def bf_scrap(html, url):
+        job_listings = []
         # Get page source and parse with BeautifulSoup
-        html = driver.page_source
         soup = BeautifulSoup(html, 'html.parser')
-        
+        # print(soup)
+
+        filename = url.split('=')[-1]
+        # save as a html
+        with open(f'{base_path}/html_files/{state}/{filename}.txt', 'w', encoding='utf-8') as file:
+            file.write(str(soup))
+
         # Find all job cards
         job_cards = soup.find_all('li', class_='data-results-content-parent')
 
@@ -46,40 +63,75 @@ def scrapping_website(url):
                 'URL': f"https://www.careerbuilder.com{job_url}",
                 "Reference": url
             })
-    except Exception as e:
-        print("Error 2:", e)
-    
-    return job_listings
+        return job_listings
 
-
-for i in range(5, 6, 1):  
-    time.sleep(30)
-    try: 
-        # Scrape the website
-        url = f"https://www.careerbuilder.com/jobs?posted=30&page_number={i}"
-        job_listings = scrapping_website(url)
-        # print(job_listings)
-
+    def save_dl(job_list):
         if os.path.exists('data/job_listings.csv'):
             # Open the CSV file in append mode and add new rows
             with open('data/job_listings.csv', 'a', newline='', encoding='utf-8') as file:
                 writer = csv.writer(file)
-                for job in job_listings:
+                for job in job_list:
                     writer.writerow(job.values())
             print("Added new rows")
         else:
             # Convert the job listings to a DataFrame and save to CSV
-            job_df = pd.DataFrame(job_listings)
+            job_df = pd.DataFrame(job_list)
             job_df.to_csv('data/job_listings.csv', index=False, encoding='utf-8')
             print("Created a new dataset and added new rows")
 
-        print(f"Website number {i} added successfully - {url}")
+    async def scrapping_website(url):
+        try:
+            pages_detail = {}
+            browser = await uc.start()
+            for index, url_ in enumerate(url):
+                time.sleep(2)
+                if index == 0:
+                    pages_detail[url_] = await browser.get(url_)
+                else:
+                    pages_detail[url_] = await browser.get(url_, new_tab=True)
+        
+            time.sleep(240)
+            
+            html_ = {}
+            # await page.save_screenshot()
+            for page_url, page in pages_detail.items():
+                html_[page_url] = await page.get_content()
 
-        time.sleep(30)
+            # print(html_)
+
+            jl_ = []
+            for page_url, html in html_.items():
+                jl_.append(bf_scrap(html, page_url))
+            print(len(jl_))
+
+        except Exception as e:
+            print("Error 2:", e)
+        
+        return jl_
+
+
+    # for i in range(1, 2, 1):  
+    # time.sleep(30)
+    try: 
+        # Scrape the website
+        url = [f"{base_url}{start_number + i}" for i in range(98)]
+        # print(url)
+        # print(url[0], url[1], url[2])
+        jl_ = uc.loop().run_until_complete(scrapping_website(url))
+
+        for jl in jl_:
+            save_dl(jl)
+
+        for i in url:
+            print(f"Number: {i.split("=")[-1]} added successfully")
+
+        start_number = int(url[-1].split("=")[-1]) + 1
+        time.sleep(5)
     except Exception as e:
         print("Error 1:", e)
 
-print("Job listings have been saved to 'data/job_listings.csv'.")
+    print("Job listings have been saved to 'data/job_listings.csv'.")
 
-# Close the WebDriver
-driver.quit()
+    time.sleep(240)
+
+
---
+++ 3_bfsoup.py
@@ -0,0 +1,38 @@
+# import asyncio
+# import nodriver as uc
+
+# async def main():
+#     browser = await uc.start()
+#     page = await browser.get('https://www.nowsecure.nl')
+
+#     # await page.save_screenshot()
+#     # await page.get_content()
+#     # await page.scroll_down(150)
+#     # elems = await page.select_all('*[src]')
+#     # for elem in elems:
+#     #     await elem.flash()
+
+#     page2 = await browser.get('https://twitter.com', new_tab=True)
+#     page3 = await browser.get('https://github.com/ultrafunkamsterdam/nodriver', new_tab=True)
+
+#     for p in (page, page2, page3):
+#        await p.bring_to_front()
+#        await p.scroll_down(200)
+#        await p   # wait for events to be processed
+#        await p.reload()
+#        if p != page3:
+#            await p.close()
+
+
+# if __name__ == '__main__':
+
+#     # since asyncio.run never worked (for me)
+#     uc.loop().run_until_complete(main())
+
+arr = [1,2,34,4]
+hasMap = {
+    "A": 23,
+    "b": 42
+}
+print(len(hasMap))
+print(hasMap)
