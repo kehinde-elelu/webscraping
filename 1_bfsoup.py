c352f51607519fdc254e43900f8d985ba828e887
kehinde-elelu <abdulsalamelelu@gmail.com>
2024-08-22 22:07:58 -0400

    scrape by state

--- 1_bfsoup.py
+++ 1_bfsoup.py
@@ -18,7 +18,7 @@ import nodriver as uc
 
 base_path = "/Users/kehindeelelu/Documents/ETS/webscraping/data"
 
-stateNames = ['Arkansas']
+stateNames = ['Montana']
 #   'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut',
 #   'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas',
 #   'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 
@@ -27,6 +27,8 @@ stateNames = ['Arkansas']
 #   'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Utah', 
 #   'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
 # ]
+
+start_, end_ = 1, 97
 
 for state in stateNames:
     os.makedirs(f'{base_path}/html_files/{state}/', exist_ok=True)
@@ -40,10 +42,10 @@ for state in stateNames:
         soup = BeautifulSoup(html, 'html.parser')
         # print(soup)
 
-        filename = url.split('=')[-1]
-        # save as a html
-        with open(f'{base_path}/html_files/{state}/{filename}.txt', 'w', encoding='utf-8') as file:
-            file.write(str(soup))
+        # filename = url.split('=')[-1]
+        # # save as a html
+        # with open(f'{base_path}/html_files/{state}/{filename}.txt', 'w', encoding='utf-8') as file:
+        #     file.write(str(soup))
 
         # Find all job cards
         job_cards = soup.find_all('li', class_='data-results-content-parent')
@@ -52,7 +54,8 @@ for state in stateNames:
             title = job_card.find('div', class_='data-results-title').text.strip() if job_card.find('div', class_='data-results-title') else None 
             company = job_card.find_all('span')[0].text.strip() if job_card.find_all('span')[0] else None 
             location = job_card.find_all('span')[1].text.strip() if job_card.find_all('span')[1] else None 
-            job_type = job_card.find('div', class_='data-details').find_all('span')[2].text.strip() if job_card.find('div', class_='data-details').find_all('span')[2] else None 
+            job_type = job_card.find('div', class_='data-details').find_all('span')[2].text.strip() if job_card.find('div', class_='data-details') and len(job_card.find('div', class_='data-details').find_all('span')) > 2 else None
+            # job_type = job_card.find('div', class_='data-details').find_all('span')[2].text.strip() if job_card.find('div', class_='data-details').find_all('span')[2] else None 
             job_url = job_card.find('a')['href'].strip() if job_card.find('a') else None 
 
             job_listings.append({
@@ -73,6 +76,7 @@ for state in stateNames:
                 for job in job_list:
                     writer.writerow(job.values())
             print("Added new rows")
+            time.sleep(2)
         else:
             # Convert the job listings to a DataFrame and save to CSV
             job_df = pd.DataFrame(job_list)
@@ -80,32 +84,41 @@ for state in stateNames:
             print("Created a new dataset and added new rows")
 
     async def scrapping_website(url):
-        try:
-            pages_detail = {}
-            browser = await uc.start()
-            for index, url_ in enumerate(url):
-                time.sleep(2)
-                if index == 0:
-                    pages_detail[url_] = await browser.get(url_)
-                else:
-                    pages_detail[url_] = await browser.get(url_, new_tab=True)
+        # try:
+        pages_detail = {}
+        browser = await uc.start()
+        for index, url_ in enumerate(url):
+            time.sleep(2)
+            if index == 0:
+                pages_detail[url_] = await browser.get(url_)
+            else:
+                pages_detail[url_] = await browser.get(url_, new_tab=True)
+    
+        time.sleep(60)
         
-            time.sleep(240)
-            
-            html_ = {}
-            # await page.save_screenshot()
-            for page_url, page in pages_detail.items():
-                html_[page_url] = await page.get_content()
+        html_ = {}
+        # await page.save_screenshot()
+        for page_url, page in pages_detail.items():
+            # time.sleep(10)
+            html_[page_url] = await page.get_content()
 
-            # print(html_)
+        # print(html_.keys())
 
-            jl_ = []
-            for page_url, html in html_.items():
-                jl_.append(bf_scrap(html, page_url))
-            print(len(jl_))
+        # Check if any values in the dictionary are empty
+        # any_empty = any(not value for value in html_.values())
+        # print(f"Is there any empty value? {any_empty}")
 
-        except Exception as e:
-            print("Error 2:", e)
+        jl_ = []
+        for page_url, html in html_.items():
+            job_extract = bf_scrap(html, page_url)
+            if not job_extract:
+                print(page_url)
+            else:
+                jl_.append(job_extract)
+        print(len(jl_))
+
+        # except Exception as e:
+        #     print("Error 2:", e)
         
         return jl_
 
@@ -114,7 +127,7 @@ for state in stateNames:
     # time.sleep(30)
     try: 
         # Scrape the website
-        url = [f"{base_url}{start_number + i}" for i in range(98)]
+        url = [f"{base_url}{start_number + i}" for i in range(start_, end_, 1)]
         # print(url)
         # print(url[0], url[1], url[2])
         jl_ = uc.loop().run_until_complete(scrapping_website(url))
@@ -132,6 +145,7 @@ for state in stateNames:
 
     print("Job listings have been saved to 'data/job_listings.csv'.")
 
-    time.sleep(240)
+    time.sleep(50)
+    print(state)
 
 
